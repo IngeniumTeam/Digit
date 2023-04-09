def5ad618e09fbfe32ebc6d64dec97ee397b3a57
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-04-09 18:43:42 +0200

    Add leading zero support

--- src/Digit.h
+++ src/Digit.h
@@ -5,7 +5,7 @@ class Digit
 {
     public:
         Digit(uint8_t iPin1, uint8_t iPin2, int brightness = 5);
-        void display(int number);
+        void display(int number, bool leadingZero = false);
         uint8_t pin1;
         uint8_t pin2;
         TM1637Display digit = TM1637Display(0, 0);
